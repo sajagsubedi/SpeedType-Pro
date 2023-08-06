c31f6ff0e08aa8cc3b9cdfcd1bc457adf0eeca13
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2023-08-06 17:47:56 +0545

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import { Header, Footer, Main } from "./Components/index";
 
+export default function App() {
+  return (
+    <div className="min-h-[100vh] bg-gray-900">
+      <Header />
+      <hr />
+      <Main />
+      <hr />
+      <Footer />
+    </div>
+  );
+}
